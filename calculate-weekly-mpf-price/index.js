9495c89fdd51c545c5439d669b032950fba184f3
Gavin Fong <gavinklfong@gmail.com>
2019-04-19 22:25:41 +0800

    enhance average price calculation by queue for weekly and monthly

---
+++ calculate-weekly-mpf-price/index.js
@@ -0,0 +1,128 @@
+const moment = require('moment');
+const aws = require('aws-sdk');
+aws.config.update({region: 'us-east-2'});
+
+// aws.config.update({
+//   region: "local",
+//   endpoint: "http://localhost:8000"
+// });
+
+const dynamodb = new aws.DynamoDB;
+const docClient = new aws.DynamoDB.DocumentClient();
+
+const DEFAULT_DAILY_TABLE_NAME = "MPFPriceDaily";
+const DEFAULT_MONTHLY_TABLE_NAME = "MPFPriceMonthly";
+const DEFAULT_WEEKLY_TABLE_NAME = "MPFPriceWeekly";
+
+exports.handler = async (event) => {
+
+    console.log("SQS event received");
+    console.log(JSON.stringify((event)));
+
+    for (let eventRecord of event.Records) {   
+        try {
+                if (eventRecord.eventSource == "aws:sqs") {
+                    const message = JSON.parse(eventRecord.body);
+                    console.log("Message: " + JSON.stringify(message)); 
+                    let averagePriceRecord = await calculateWeeklyAveragePrice(message.trusteeSchemeFundId, message.priceDate);
+                    await saveFundPrice(averagePriceRecord, DEFAULT_WEEKLY_TABLE_NAME);
+              } else {
+                  throw new Error('Unknown event: ' + eventRecord.eventSource);
+              }
+        
+        } catch (e) {
+            console.error(e);
+            throw e;
+        }
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Completed.')
+    };
+    return response;
+};
+
+async function calculateWeeklyAveragePrice(trusteeSchemeFundId, startOfDate) {
+
+   // calculate average price
+   let startOfDateMoment = moment(startOfDate).startOf('week');
+   let endOfDateMoment = moment(startOfDateMoment).endOf('week');
+    
+    console.log("## calculate weekly average price for id = "  + trusteeSchemeFundId + ", startOfDatePeriod = " + startOfDateMoment.format());
+
+    let params = {
+        TableName: "MPFPriceDaily",
+        // IndexName: "trustee_date_index",
+        KeyConditionExpression: "trusteeSchemeFundId = :id and priceDate between :startDate and :endDate",
+        // FilterExpression: "fundName = :fundName",
+        ExpressionAttributeValues: {
+            ":id": trusteeSchemeFundId,
+            ":startDate": startOfDateMoment.valueOf(),
+            ":endDate": endOfDateMoment.valueOf(),
+                // ":fundName": "HSI"
+            },
+        ProjectionExpression: "trusteeSchemeFundId, trustee, scheme, fundName, priceDate, dateDisplay, price"
+    };
+
+    let mpfPriceRecord = {};
+    let sum = 0;
+    let count = 0;
+    let averagePrice = 0;
+
+    try {
+        const queryData = await docClient.query(params).promise();
+        console.log("Query succeeded.");
+        queryData.Items.forEach(function(item) {
+            // console.log(JSON.stringify(item));
+            sum = sum + +item.price;
+            count++;
+        });
+
+        if (count > 0) averagePrice = sum / count;
+        console.log("average price = " + averagePrice + ", count = " + count + ", sum = " + sum);
+        mpfPriceRecord = {"trusteeSchemeFundId": trusteeSchemeFundId, "trustee": queryData.Items[0].trustee, "scheme": queryData.Items[0].scheme, "fundName": queryData.Items[0].fundName, "priceDate": startOfDateMoment.valueOf(), "priceDateDisplay": startOfDateMoment.format("YYYY-MM-DD"), "price": averagePrice};
+
+        console.log("mpfPriceRecord: " + JSON.stringify(mpfPriceRecord));
+
+    } catch (e) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    }
+
+    return mpfPriceRecord;
+}
+
+async function saveFundPrice(fundPriceRecord, tableName) {
+
+    if (!tableName) {
+        tableName = DEFAULT_MONTHLY_TABLE_NAME;
+    }
+  
+    if (!fundPriceRecord.trusteeSchemeFundId) {
+        fundPriceRecord.trusteeSchemeFundId = fundPriceRecord.trustee + "-" + fundPriceRecord.scheme + "-" + fundPriceRecord.fundName;
+    }
+  
+    let params = {
+        TableName: tableName,
+        Item: fundPriceRecord
+    };
+    console.log("Calling PutItem");
+    console.log(JSON.stringify(params));
+  
+    let result = new Promise((resolve, reject) =>  {
+        
+        docClient.put(params, function(err, data) {
+            if (err)  { 
+                console.error(err); // an error occurred
+                reject(err);
+            }
+            else { 
+                console.log("PutItem returned successfully");
+                resolve(fundPriceRecord.trusteeSchemeFundId);
+            }
+        });
+    });
+  
+    return await result;
+  
+  }
---
+++ distribute-for-average-price-calculation/index.js
@@ -0,0 +1,172 @@
+const moment = require('moment');
+const aws = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+
+aws.config.update({region: 'us-east-2'});
+
+// aws.config.update({
+//   region: "local",
+//   endpoint: "http://localhost:8000"
+// });
+
+// const dynamodb = new aws.DynamoDB;
+// const docClient = new aws.DynamoDB.DocumentClient();
+
+const sqs = new aws.SQS();
+
+const DEFAULT_DAILY_TABLE_NAME = "MPFPriceDaily";
+const DEFAULT_MONTHLY_TABLE_NAME = "MPFPriceMonthly";
+const DEFAULT_WEEKLY_TABLE_NAME = "MPFPriceWeekly";
+
+const DEFAULT_MONTHLY_PRICE_QUEUE = "mpf-price-service-monthly-mpf-price-queue-dev";
+const DEFAULT_WEEKLY_PRICE_QUEUE = "mpf-price-service-weekly-mpf-price-queue-dev";
+
+exports.handler = async (event) => {
+
+    if (!process.env.MONTHLY_PRICE_QUEUE || !process.env.WEEKLY_PRICE_QUEUE) {
+        throw new Error('MONTHLY_PRICE_QUEUE or WEEKLY_PRICE_QUEUE is missing in env variable');
+    }
+
+    console.log("Dynamodb stream event received");
+    console.log(JSON.stringify((event)));
+
+    let weeklyDateMap = new Map();
+    let monthlyDateMap = new Map();
+
+
+    for (let eventRecord of event.Records) {   
+        try {
+              if (eventRecord.eventSource == "aws:dynamodb") {
+              
+                    let trusteeSchemeFundId = eventRecord.dynamodb.Keys.trusteeSchemeFundId.S;
+                    let priceDate = +eventRecord.dynamodb.Keys.priceDate.N;
+
+                    // Update weekly date key
+                    weeklyDateMap = consolidateWeeklyDate(weeklyDateMap, trusteeSchemeFundId, priceDate);
+
+                    // Update monthly date key
+                    monthlyDateMap = consolidateMonthlyDate(monthlyDateMap, trusteeSchemeFundId, priceDate);
+              }
+        
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    let weeklyRecords = await sendToWeeklyPriceRecordQueue(weeklyDateMap);
+    let monthlyRecords = await sendToMonthlyPriceRecordQueue(monthlyDateMap);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Completed. Weekly record processed = ' + weeklyRecords.length + ', Monthly record processed = ' + monthlyRecords.length)
+    };
+    return response;
+};
+
+
+async function sendQueueMessage(record, queueUrl) {
+    let params = {
+        // DelaySeconds: 10,
+        MessageBody: JSON.stringify(record),
+        QueueUrl: queueUrl
+      };
+
+    await sqs.sendMessage(params).promise();
+}
+
+async function sendToWeeklyPriceRecordQueue(fundDateMap) {
+    let priceRecords = [];
+
+    for (let trusteeSchemeFundId of fundDateMap.keys()) {
+        console.log("weeklyDateMap - " + trusteeSchemeFundId);
+        let datePeriodSet = fundDateMap.get(trusteeSchemeFundId);
+        for (let datePeriod of datePeriodSet) {
+            console.log("datePeriodSet - " + datePeriod);
+            let priceRecord = {'trusteeSchemeFundId': trusteeSchemeFundId, 'priceDate': datePeriod};
+            priceRecords.push(priceRecord);
+            sendQueueMessage(priceRecord, process.env.WEEKLY_PRICE_QUEUE);
+        }
+    }
+
+    return priceRecords;
+}
+
+async function sendToMonthlyPriceRecordQueue(fundDateMap) {
+
+    let priceRecords = [];
+
+    for (let trusteeSchemeFundId of fundDateMap.keys()) {
+        console.log("monthlyDateMap - " + trusteeSchemeFundId);
+        let datePeriodSet = fundDateMap.get(trusteeSchemeFundId);
+        for (let datePeriod of datePeriodSet) {
+            console.log("datePeriodSet - " + datePeriod);
+            let priceRecord = {'trusteeSchemeFundId': trusteeSchemeFundId, 'priceDate': datePeriod};
+            priceRecords.push(priceRecord);
+            sendQueueMessage(priceRecord, process.env.MONTHLY_PRICE_QUEUE);
+        }
+    }
+
+    return priceRecords;
+}
+
+
+function consolidateWeeklyDate(fundDateMap, trusteeSchemeFundId, priceDate) {
+    let weeklyDateSet = fundDateMap.get(trusteeSchemeFundId);
+    if (!weeklyDateSet) {
+        weeklyDateSet = new Set();
+    }
+    let weekStartDate = moment(priceDate);
+    weeklyDateSet.add(weekStartDate.startOf('week').valueOf());
+    fundDateMap.set(trusteeSchemeFundId, weeklyDateSet);
+
+    return fundDateMap;
+}
+
+
+function consolidateMonthlyDate(fundDateMap, trusteeSchemeFundId, priceDate) {
+    let monthlyDateSet = fundDateMap.get(trusteeSchemeFundId);
+    if (!monthlyDateSet) {
+        monthlyDateSet = new Set();
+    }
+    let monthStartDate = moment(priceDate);
+    monthlyDateSet.add(monthStartDate.startOf('month').valueOf());
+    fundDateMap.set(trusteeSchemeFundId, monthlyDateSet);
+
+    return fundDateMap;
+}
+
+function initSqsWriteStream (queueName) {
+
+    if (!queueName) {
+        console.log("QueueName in environment variable is empty, use default value"); 
+
+        queueName = DEFAULT_SQS_QUEUE_NAME;
+    }
+
+    console.log("QueueName: " + queueName); 
+
+    let sqsStream = new SqsWriteStream(
+        {name: queueName},
+        {batchSize: 10}
+    );
+
+    sqsStream.on("Sqs msgReceived", (data) => {
+            console.log("sqs msgReceived: " + JSON.stringify(data));
+    });
+
+    sqsStream.on("Sqs msgProcessed", (data) => {
+            console.log("sqs msgProcessed: " + JSON.stringify(data));
+    });
+
+    let sqsEnd = new Promise(function(resolve, reject) {
+        sqsStream.on('finish', () => {
+        console.log('All SQS writes are now complete.');
+        resolve("Finished");
+        });
+        sqsStream.on('error', reject); 
+    });
+
+    let sqsWriteStream = {stream: sqsStream, finishPromise: sqsEnd};
+
+    return sqsWriteStream;
+}
